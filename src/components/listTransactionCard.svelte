38dc10495103b5f09049f9caf6a4c09c3fee3032
AkashFestivs <gadhaveakash999@gmail.com>
2023-03-30 01:36:45 +0530

    commit

--- src/components/listTransactionCard.svelte
+++ src/components/listTransactionCard.svelte
@@ -1,19 +1,94 @@
 <script>
 
+let completed = '#00FA9A';
+let failed = '#F08080';
+let pending = '#FAFAD2';
+
 </script>
 
 <div class="listTranscationCard">
-
+    <div class="listTransactionHeader row">
+        <div class="row" style="font-size: 25px;">
+            <div class="col-md-6">
+                <b>Transaction List</b>
+            </div>
+            <div class="col-md-6" style="gap: 50px; display: flex; justify-content: end;">
+                <b>75</b>
+                <b>15</b>
+                <b>7</b>
+            </div>
+        </div>
+        <div class="row" style="padding: 20px; height: 30px; font-size: 10px; border-radius: 30px;">
+            <div class="col-md-6" style="height: 100%; display: flex;justify-content: start; align-items: end;">
+                <b>Total Transaction : 225</b>
+            </div>
+            <div class="col-md-6"  style="height: 100%; display: flex;justify-content: end; align-items: end; gap:27px">
+                <b>Colmpeted</b>
+                <b>Failed</b>
+                <b>Pending</b>
+            </div>
+        </div>
+    </div>
+    <div class="transactionBody">
+        <table class="table table-responsive"> 
+            <thead class="table-light">
+                <tr>
+                    <td>ACCOUNT</td>
+                    <td>Transaction Ammount</td>
+                    <td>Status</td>
+                    <td>Time</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>CASH</td>
+                    <td>400 &#x20b9;</td>
+                    <td><i style="background-color: {completed};" > &#128338;  COMPLETED </i></td>
+                    <td>23 Feb 2023 17:45:31</td>
+                </tr>
+                <tr>
+                    <td>CASH</td>
+                    <td>400 &#x20b9;</td>
+                    <td><i style="background-color: {failed};" > &#128338;  FAILED</i></td>
+                    <td>23 Feb 2023 17:45:31</td>
+                </tr>
+                <tr>
+                    <td>CASH</td>
+                    <td>400 &#x20b9;</td>
+                    <td><i style="background-color: {pending};" > &#128338;  PENDING </i></td>
+                    <td>23 Feb 2023 17:45:31</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </div>
 
 
 <style>
+
+i{
+    border-radius: 8px;
+}
 
 .listTranscationCard{
     margin-top: 20px;
     height: 400px;
     background-color: #FFFFFF;
     border-radius: 20px;
+
+}
+
+.listTransactionHeader{
+    padding: 20px;
+    font-family: unset;
+    display: flex;
+    justify-content: center;
+}
+
+.transactionBody{
+    padding: 20px;
+    font-family: unset;
+    text-align: center;
 
 }
 
--- src/components/paymentCard.svelte
+++ src/components/paymentCard.svelte
@@ -3,9 +3,15 @@
 </script>
 
 <div class="paymentCard">
-    <div class="paymentCircle">
-
-    </div>
+        <div class="paymentCircle">
+            <img src="icons\logo\electricity.jpg" alt="" />
+        </div>
+        <div class="paymentCircle">
+            <img src="icons\logo\mobileRecgarge.jpg" alt="" />
+        </div>
+        <div class="paymentCircle">
+            <img src="icons\logo\gas.jpg" alt="" />
+        </div>
 </div>
 
 
@@ -15,10 +21,33 @@
     height: 120px;
     background-color: #FFFFFF;
     border-radius: 20px;
+    padding: 10px;
+    display: flex;
+    align-items: center;
+    gap: 15px;
 }
 
 .paymentCircle{
-    
+    width: 100px;
+    height: 100px;
+    border-radius: 20px;
+    display: flex;
 }
+
+.paymentCircle img{
+    align-self: center;
+    width: 100%;
+    border-radius: 50px;
+
+}
+
+.paymentCircle:hover{
+    box-shadow: 0px 0px 5px 1px #B5C2C9;
+    width: 100px;
+    height: 100px;
+    border-radius: 40px;
+}
+
+
 
 </style>
